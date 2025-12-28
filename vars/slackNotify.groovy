570a5ff6ef3db3f7d5695cc4416cf1141a497dff
Priya Yellela <Lakshmi.Yellela@Triconinfotech.com>
2025-12-28 23:49:34 +0530

    Update slackNotify.groovy

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,7 +1,1 @@
-def call(String status) {
-    slackSend(
-        channel: '#ci-cd-alerts',
-        color: status == 'SUCCESS' ? 'good' : 'danger',
-        message: "Pipeline ${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
-    )
-}
+
